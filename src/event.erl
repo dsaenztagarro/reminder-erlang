b1caebf4348cb1c22d89f6750ba82440ac4115c1
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-14 00:26:27 +0100

    Adding an interface

--- src/event.erl
+++ src/event.erl
@@ -4,11 +4,43 @@
                 name="",
                 to_go=0}).
 
-loop(S = #state{server=Server}) ->
+%% Loop uses a list for times in order to go around the ~49 days limit on
+%% timeouts
+loop(S = #state{server=Server, to_go=[T|Next]}) ->
   receive
     {Server, Ref, cancel} ->
       Server ! {Ref, ok}
-  after S#state.to_go*1000 ->
-    Server ! {done, S#state.name}
+  after T*1000 ->
+    if Next =:= [] ->
+         Server ! {done, S#state.name};
+       Next =/= [] ->
+         loop(S#state{to_go=Next})
+    end
   end.
 
+%% Because Erlang is limited to about 49 days (49*24*60*60*1000) in
+%% milliseconds, the following function is used.
+normalize(N) ->
+  Limit = 49*24*60*60,
+  [N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+start(EventName, Delay) ->
+  spawn(?MODULE, init, [self(), EventName, Delay]).
+
+start_link(EventName, Delay) ->
+  spawn_link(?MODULE, init, [self(), EventName, Delay]).
+
+init(Server, EventName, Delay) ->
+  loop(#state{server=Server, name=EventName, to_go=normalize(Delay)}).
+
+cancel(Pid) ->
+  %% Monitor in case the process is already dead.
+  Ref = erlang:monitor(process, Pid),
+  Pid ! {self(), Ref, cancel},
+  receive
+    {Ref, ok} ->
+      erlang:demonitor(Ref, [flush]),
+      ok;
+    {'DOWN', Ref, process, Pid, _Reason} ->
+      ok
+  end.
