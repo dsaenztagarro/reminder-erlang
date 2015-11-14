3d69b3f1e3b9b4aba6db946736ec7f487e324339
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-14 23:28:32 +0100

    Hide messages

--- src/evserv.erl
+++ src/evserv.erl
@@ -41,7 +41,28 @@ loop(S = #state{}) ->
                    S#state.events
                end,
       Pid ! {MsgRef, ok},
-      loop(S#state{events=Events})
+      loop(S#state{events=Events});
+    {done, Name} ->
+      case orddict:find(Name, S#state.events) of
+        {ok, E} ->
+          send_to_clients({done, E#event.name, E#event.description},
+                          S#state.clients),
+          NewEvents = orddict:erase(Name, S#state.events),
+          loop(S#state{events=NewEvents});
+        error ->
+          %% This may happen if we cancel an event and it fires at the same time
+          loop(S)
+      end;
+    shutdown ->
+      exit(shutdown);
+    {'DOWN', Ref, process, _Pid, _Reason} ->
+      loop(S#state{clients=orddict:erase(Ref, S#state.clients)});
+    code_change ->
+      ?MODULE:loop(S);
+    Unknown ->
+      io:format("Unknown message: ~p~n", [Unknown]),
+      loop(S)
+
   end.
 
 init() ->
@@ -68,3 +89,57 @@ valid_time(H,M,S) when H >= 0, H < 24,
                        M >= 0, M < 60,
                        S >= 0, S < 60 -> true;
 valid_time(_,_,_) -> false.
+
+send_to_clients(Msg, ClientDict) ->
+  orddict:map(fun(_Ref, Pid) -> Pid ! Msg end, ClientDict).
+
+%% Hide messages
+
+start() ->
+  register(?MODULE, Pid=spawn(?MODULE, init, [])),
+  Pid.
+
+start_link() ->
+  register(?MODULE, Pid=spawn_link(?MODULE, init, [])),
+  Pid.
+
+terminate() ->
+  ?MODULE ! shutdown.
+
+subscribe(Pid) ->
+  Ref = erlang:monitor(process, whereis(?MODULE)),
+  ?MODULE ! {self(), Ref, {subscribe, Pid}},
+  receive
+    {Ref, ok} ->
+      {ok, Ref};
+    {'DOWN', Ref, process, _Pid, Reason} ->
+      {error, Reason}
+  after 5000 ->
+          {error, timeout}
+  end.
+
+add_event(Name, Description, TimeOut) ->
+  Ref = make_ref(),
+  ?MODULE ! {self(), Ref, {add, Name, Description, TimeOut}},
+  receive
+    {Ref, Msg} -> Msg
+  after 5000 ->
+          {error, timeout}
+  end.
+
+cancel(Name) ->
+  Ref = make_ref(),
+  ?MODULE ! {self(), Ref, {cancel, Name}},
+  receive
+    {Ref, ok} -> ok
+  after 5000 ->
+          {error, timeout}
+  end.
+
+listen(Delay) ->
+  receive
+    M = {done, _Name, _Description} ->
+      [M | listen(0)]
+  after Delay*1000 ->
+          []
+  end.
